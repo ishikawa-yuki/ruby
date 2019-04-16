d2bf148dca9fe15554cdcc7c77fafa3cc069c1d1
石河優貴 <burle42820@gmail.com>
2019-04-16 10:06:44 +0900

    現在の数値を表示する箇所をまとめる変更を行いました

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,16 +4,15 @@
 =end
 1.upto(100){|i|
 
-case
-when (i%15 == 0)
     print i
-    puts " FIZZBUZZ"
-when (i%5 == 0)
-    print i
-    puts " BUZZ"
-when (i%3 == 0)
-    print i
-    puts " FIZZ"
-else puts i
-end
+
+    case
+      when (i%15 == 0)
+        puts " FIZZBUZZ"
+      when (i%5 == 0)
+        puts " BUZZ"
+      when (i%3 == 0)
+        puts " FIZZ"
+      else puts
+    end
 }
