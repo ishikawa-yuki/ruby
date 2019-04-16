86a930385457c736e86b38eb798d93634f3874eb
石河優貴 <burle42820@gmail.com>
2019-04-16 13:02:20 +0900

    計算部分をメソッド化

--- gojoho.rb
+++ gojoho.rb
@@ -1,25 +1,31 @@
-＝begin
+=begin
 ユークリッドの互除法を用いて最大公約数をだす
 =end
 
-print "一つ目"
-
-x = gets.to_i
-
-print "二つ目"
-
-y = gets.to_i
-
-while(x>=1 and y>=1)
-    if x >= y then
-        x = x % y
+def keisan(a,b)
+    while(a>=1 and b>=1)
+        if a >= b then
+            a = a % b
+        else
+            b = b % a
+        end
+    end
+    if a==0 then
+        return b
     else
-        y = y % x
+        return a
     end
 end
 
-if x==0 then
-    puts "最大公約数は" + y.to_s + "です"
-else
-    puts "最大公約数は" + x.to_s + "です"
-end
+print "一つ目の値"
+
+x = gets.to_i
+
+print "二つ目の値"
+
+y = gets.to_i
+
+ans = keisan(x,y)
+
+puts "最大公約数は" + ans.to_s + "です"
+
