444a0bd574d723ef6de8c6590223189f4bbb299b
石河優貴 <burle42820@gmail.com>
2019-04-17 10:20:53 +0900

    出現回数の処理部分の変更・コメントの追加・不要な変数・不要なメソッドの消去

--- mojiretu.rb
+++ mojiretu.rb
@@ -1,16 +1,14 @@
-
-
+=begin
+入力した文字列から出現頻度がもっとも多い文字と出現した回数を表示する
+=end
 print "文字列を入力してください"
 
-s=gets.chomp.to_s
-
-i=s.length
+s=gets.chomp
 
 mojiretu =Hash.new
 
 mcnt = 0
 mchar = ""
-
 
 s.each_char{|s| 
     if mojiretu.include?(s) then
@@ -18,19 +16,15 @@ s.each_char{|s|
     else
         mojiretu[s] = 1
     end
-    char = s
-    cnt = mojiretu[s]
-    if mcnt <= cnt
-        if mcnt == cnt
+    if mcnt < mojiretu[s]
+        mcnt = mojiretu[s]
+        mchar = s
+    else
+        if mcnt == mojiretu[s]
             mchar += "・"+s
-        else
-            mchar = s
         end
-        mcnt = cnt
     end
 }
-
-
 
 print "もっとも出現頻度が多かった文字は#{mchar}で#{mcnt}回です"
 
